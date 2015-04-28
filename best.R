0300fdcd295338ead97ae1bd19aeb6a12a880213
Enrico Fasani <Konko74@gmail.com>
2015-04-28 13:06:30 +0200

    versione defintiva di best

---
+++ best.R
@@ -0,0 +1,53 @@
+best<-function(state,outcome)
+{
+  outcomeColumn<-0
+  
+  ##check parametrs
+  parameterOk<-0
+  if (outcome=="heart attack")
+  {
+    outcomeColumn<-11
+    parameterOk<-1
+  }
+  if (outcome=="heart failure")
+  {
+    outcomeColumn<-17
+    parameterOk<-1
+  }
+  if (outcome=="pneumonia")
+  {
+    outcomeColumn<-23
+    parameterOk<-1
+  }
+  if (parameterOk==0)
+  {
+    stop("invalid outcome")
+  }
+  
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  stateList<-subset(data,State==state,select=State)
+  if(summary(stateList)[1] == "Length:0          ")
+  {
+    stop("invalid state")
+  }
+  
+  outcomeColumnName<-colnames(data)[outcomeColumn]
+  hospitalNameColumn<-2
+  hospitalNameColumnName<-colnames(data)[hospitalNameColumn]
+  
+  hospitals<-subset(data,State==state,select=c(hospitalNameColumnName,outcomeColumnName))
+  values <- as.numeric(hospitals[, 2])
+  hospitalsName <- hospitals[, 1]
+  orderedHospitals<-hospitals[order(values,hospitalsName),]
+  
+  ris<-vector(mode="character",length=30)
+  
+  i<-1
+  while(i<=30)
+  {
+    ris[i]<-orderedHospitals[i,1]
+    i<-i+1
+  }
+  
+  return(ris)
+}
---
+++ test.R
@@ -0,0 +1,9 @@
+setwd("C:/Users/AICEnricoF/git/ProgrammingAssignment3")
+
+source("best.R")
+best("TX", "heart attack")[1]
+best("TX", "heart failure")[1]
+best("MD", "heart attack")[1]
+best("MD", "pneumonia")[1]
+best("BB", "heart attack")
+best("NY", "hert attack")
