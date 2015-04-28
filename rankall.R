c092358950d7f1b53ae405810b2705f670516705
Enrico Fasani <Konko74@gmail.com>
2015-04-28 15:47:13 +0200

    versione definitiva rankall

---
+++ rankall.R
@@ -0,0 +1,27 @@
+source("rankhospital.R")
+
+rankall<-function(outcome,num="best")
+{
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  states<-subset(data,select=State)
+
+  statesName <- states[, 1]
+  orderedStates<-states[order(statesName),]
+  
+  uniqueOrderedStates<-unique(orderedStates)
+  numberOfStates<-length(uniqueOrderedStates)
+  
+  x<-matrix(nrow=numberOfStates,ncol=2,dimnames = list(uniqueOrderedStates,c("hospital", "state")))
+  
+  ris<-vector(mode="character",1)
+  indice<-1
+  while(indice<=numberOfStates)
+  {
+    ris<-rankhospital(uniqueOrderedStates[indice], outcome, num)
+    x[indice,1] = ris[1]
+    x[indice,2] = uniqueOrderedStates[indice]
+    indice<-indice+1
+  }
+  
+  return(x)
+}
